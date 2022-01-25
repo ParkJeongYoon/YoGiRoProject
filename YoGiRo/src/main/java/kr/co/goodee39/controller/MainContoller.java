056224a36ba2b550ff46072af2cdf566610f4923
ofjj <ofjbean@gmail.com>
2022-01-25 23:17:50 +0900

    Revert "Community1"
    This reverts commit c378ec384cc8bdf114803ec7845a0f54f5e3f9f0.

--- YoGiRo/src/main/java/kr/co/goodee39/controller/MainContoller.java
+++ YoGiRo/src/main/java/kr/co/goodee39/controller/MainContoller.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @Controller
 public class MainContoller {
-	
+
 	@GetMapping("/qna/qna-main")
 	public String qnA() {
 		return "/qna/qna-main";
@@ -40,10 +39,10 @@ public class MainContoller {
 		return "/incategories/course/course-main";
 	}
 	
-//	@GetMapping("/incategories/community/community-main")
-//	public String community() {
-//		return "/incategories/community/community-main";
-//	}
+	@GetMapping("/incategories/community/community-main")
+	public String community() {
+		return "/incategories/community/community-main";
+	}
 	
 	
 	
