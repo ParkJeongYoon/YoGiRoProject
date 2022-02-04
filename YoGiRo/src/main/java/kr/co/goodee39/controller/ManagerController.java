77a2f35fcb94e012737f004b5b0d2a7f1f211e5b
zeoze <zeoze@DESKTOP-MVBFIQ6>
2022-02-04 13:51:10 +0900

    관리자 controller 수정

--- YoGiRo/src/main/java/kr/co/goodee39/controller/ManagerController.java
+++ YoGiRo/src/main/java/kr/co/goodee39/controller/ManagerController.java
@@ -11,23 +11,6 @@ public class ManagerController {
 		return "/manager/manager1/manager-chart";
 	}
 	
-	
-	
-//	@GetMapping("/manager/manager3/manager-member")
-//	public String member() {
-//		return "/manager/manager3/manager-member";
-//	}
-	
-//	@GetMapping("/manager/manager4/manager-block-member")
-//	public String block() {
-//		return "/manager/manager4/manager-block-member";
-//	}
-	
-//	@GetMapping("/manager/manager5/manager-community")
-//	public String community() {
-//		return "/manager/manager5/manager-community";
-//	}
-	
 	@GetMapping("/manager/manager6/manager-delete-community")
 	public String delete() {
 		return "/manager/manager6/manager-delete-community";
