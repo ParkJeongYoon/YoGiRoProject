c310f8f7ecd710cc681e8cad27cb0bf63dcbdc39
zeoze <zeoze@DESKTOP-MVBFIQ6>
2022-02-04 13:46:25 +0900

    manager컨트롤 수정

--- YoGiRo/src/main/java/kr/co/goodee39/controller/ManagerController.java
+++ YoGiRo/src/main/java/kr/co/goodee39/controller/ManagerController.java
@@ -11,25 +11,22 @@ public class ManagerController {
 		return "/manager/manager1/manager-chart";
 	}
 	
-//	@GetMapping("/manager/manager2/manager-que")
-//	public String question() {
-//		return "/manager/manager2/manager-que";
+	
+	
+//	@GetMapping("/manager/manager3/manager-member")
+//	public String member() {
+//		return "/manager/manager3/manager-member";
 //	}
 	
-	@GetMapping("/manager/manager3/manager-member")
-	public String member() {
-		return "/manager/manager3/manager-member";
-	}
+//	@GetMapping("/manager/manager4/manager-block-member")
+//	public String block() {
+//		return "/manager/manager4/manager-block-member";
+//	}
 	
-	@GetMapping("/manager/manager4/manager-block-member")
-	public String block() {
-		return "/manager/manager4/manager-block-member";
-	}
-	
-	@GetMapping("/manager/manager5/manager-community")
-	public String community() {
-		return "/manager/manager5/manager-community";
-	}
+//	@GetMapping("/manager/manager5/manager-community")
+//	public String community() {
+//		return "/manager/manager5/manager-community";
+//	}
 	
 	@GetMapping("/manager/manager6/manager-delete-community")
 	public String delete() {
